38f4373c9e8901ef6af3e8b2688600635ad8b19d
Kendrick Mausolf <kmausolf@ucsd.edu>
2017-06-05 19:40:12 -0700

    Kendrick: Forgot to include our schema!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503232028) do
+ActiveRecord::Schema.define(version: 20170606021506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20170503232028) do
     t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
   end
 
+  create_table "new_purchases", force: :cascade do |t|
+    t.integer  "user_id",                                    null: false
+    t.integer  "product_id",                                 null: false
+    t.integer  "quantity",                                   null: false
+    t.datetime "time",       default: '2017-05-10 20:47:13', null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.index ["product_id"], name: "index_new_purchases_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_new_purchases_on_user_id", using: :btree
+  end
+
   create_table "products", force: :cascade do |t|
     t.integer  "category_id",             null: false
     t.string   "unique_name",             null: false
@@ -53,8 +64,18 @@ ActiveRecord::Schema.define(version: 20170503232028) do
     t.datetime "time",       default: '2017-05-10 20:47:13', null: false
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
-    t.index    ["user"], name: "index_purchases_on_user", using: :btree
-    t.index    ["product"], name: "index_purchases_on_product", using: :btree
+    t.index ["product"], name: "index_purchases_on_product", using: :btree
+    t.index ["user"], name: "index_purchases_on_user", using: :btree
+  end
+
+  create_table "top50s", force: :cascade do |t|
+    t.string   "state",        null: false
+    t.string   "product_name", null: false
+    t.integer  "category_id",  null: false
+    t.integer  "total",        null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["category_id"], name: "index_top50s_on_category_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -71,5 +92,8 @@ ActiveRecord::Schema.define(version: 20170503232028) do
   add_foreign_key "carts", "products"
   add_foreign_key "carts", "users"
   add_foreign_key "categories", "users"
+  add_foreign_key "new_purchases", "products"
+  add_foreign_key "new_purchases", "users"
   add_foreign_key "products", "categories"
+  add_foreign_key "top50s", "categories"
 end
