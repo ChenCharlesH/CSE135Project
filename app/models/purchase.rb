99040cb1b91c581cf6dfa4659d34daf7e6f94a33
Kendrick Mausolf <kmausolf@ucsd.edu>
2017-06-06 16:45:29 -0700

    Kendrick: Triggers still not working

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase < ApplicationRecord
   trigger.after(:insert) do
-    "INSERT INTO new_purchases(user,product,quantity,time)
-     VALUES(NEW.user,NEW.product,NEW.quantity,NEW.time)"
+    "INSERT INTO new_purchases(user,product,quantity,time,created_at,updated_at)
+     VALUES(NEW.user,NEW.product,NEW.quantity,NEW.time,NEW.created_at,NEW.updated_at)"
   end
 end
