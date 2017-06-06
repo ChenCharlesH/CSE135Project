d97c34c598cfaeee1d0b7c54b8cf1911dbad28a2
Kendrick Mausolf <kmausolf@ucsd.edu>
2017-06-05 19:39:27 -0700

    Kendrick: Initial commit of top50, our precomputed table

--- db/migrate/20170606013025_create_new_purchases.rb
+++ db/migrate/20170606013025_create_new_purchases.rb
@@ -1,8 +1,8 @@
 class CreateNewPurchases < ActiveRecord::Migration[5.0]
   def change
     create_table :new_purchases do |t|
-      t.integer :user, null: false
-      t.integer :product, null: false
+      t.references :user, foreign_key: true, null: false
+      t.references :product, foreign_key: true, null: false
       t.integer :quantity, null: false
       t.datetime :time, null: false, default: '2017-05-10 20:47:13'
 
---
+++ test/models/top50_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Top50Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
