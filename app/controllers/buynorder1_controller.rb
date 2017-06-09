4c62f0b291b3e1401956a57f25d19905f2785984
Kendrick Mausolf <kmausolf@ucsd.edu>
2017-06-08 23:41:46 -0700

    Kendrick: Added buynorders1 for the interview

---
+++ app/controllers/buynorder1_controller.rb
@@ -0,0 +1,35 @@
+class Buynorder1Controller < ApplicationController
+  before_filter :authorize_owner
+
+  def index()
+
+  end
+
+  # Generate N orders
+  def generate()
+    # Grab 10 random users
+    @random_users = User.order("RANDOM()").limit(10)
+
+    # For each user, purchase 5 random products of quantity 1-10
+    @random_users.each do |u|
+      @random_prods = Product.order("RANDOM()").limit(5)
+
+      @random_prods.each do |r|
+        quant = 100000
+        @neworder = Purchase.new(user: u.id, product: r.id, quantity: quant)
+
+        # Attempt to commit the new order
+        if @neworder.save
+          next
+        else
+          flash[:alert] = @neworder.errors.full_messages.to_sentence
+          render action: :index
+        end #end if
+      end #end products for each
+    end #end users for each
+
+    # If all went well, redirect and report
+    redirect_to "/buynorder", flash: {notice:"Generated 10 orders!"}
+
+  end #end generate
+end
